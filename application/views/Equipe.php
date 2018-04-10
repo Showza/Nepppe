739523a42f8157809eca53e931b4641dd2aa531f
CamilaGit <camilatropia95@gmail.com>
2018-04-09 21:13:55 -0300

    Página com toda a equipe

--- application/views/Equipe.php
+++ application/views/Equipe.php
@@ -1,11 +1,44 @@
-<div class="container"> 
-    <div class="row">
-        <div class="col s12 m12 l12">
-            <h1 class="red-text">Equipe</h1>
-            <p>
-            	Lorem ipsum faucibus convallis torquent volutpat neque ultricies varius euismod, convallis quisque fringilla fames placerat elit quam torquent, velit at nulla dictum porttitor vehicula mi nisl. conubia nisi aenean habitant lobortis suscipit duis taciti nisi aliquet curabitur bibendum vestibulum habitant torquent, magna suspendisse imperdiet mattis tempus pharetra ipsum libero sapien amet vulputate taciti. lorem augue facilisis dictum himenaeos etiam praesent aenean semper aptent phasellus litora quisque, taciti hendrerit velit porttitor posuere lacinia eget luctus consectetur elit est. interdum sit sociosqu pellentesque adipiscing ultricies lacinia nisl etiam, cras cubilia nisl vulputate quam molestie elit vivamus suspendisse, suscipit sollicitudin lectus inceptos id diam vitae. 
-            	Risus vestibulum erat platea primis curabitur aliquam amet fermentum enim vehicula, morbi accumsan nostra ut cras augue posuere blandit sagittis, sollicitudin tellus himenaeos sociosqu primis feugiat malesuada vulputate cursus. sociosqu aliquet tempus accumsan vestibulum lectus arcu habitant urna, fermentum sagittis turpis praesent lobortis nec. venenatis imperdiet duis pharetra tellus tristique nullam quisque erat felis, tortor fames velit magna eros suspendisse lectus etiam, augue aliquam enim curabitur lacus interdum pellentesque cubilia. neque curabitur vel vulputate lorem volutpat id egestas tempor, pretium porta pharetra ipsum nec per sollicitudin netus porta, purus duis urna at elit ut magna. 
-            </p>
-        </div>
-    </div>
-</div>
+<div class="container" style="margin-top: 30px; margin-right: 30px;">
+		<div class="row">
+			<div class="col">
+				<p><h3>Pesquisadores</h3></p>
+				<ul>
+					<li>Zara Figueiredo Tripodi - UFOP - <a href="http://lattes.cnpq.br/3705451568314846">lattes</a></li>
+					<li>Veronica Mendes Pereira - UFOP - <a href="http://lattes.cnpq.br/7345545374336076">lattes</a></li>
+					<li>Marcelo Donizete da Silva - UFOP - <a href="http://lattes.cnpq.br/7560421514245453">lattes</a></li>
+					<li>Thiago Alves - UFPR- <a href="http://lattes.cnpq.br/7802091709644694#">lattes</a></li>
+					<li>José Lucas Pedreira Bueno - UFTM - <a href="http://lattes.cnpq.br/0805268924348920">lattes</a></li>
+					<li>Breynner Ricardo de Oliveira - UFOP - <a href="http://lattes.cnpq.br/8368917322952740">lattes</a></li>
+				</ul>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col">
+				<p><h3>Alunos de mestrado</h3></p>
+				<ul>
+					<li>Eliane das Graças Pereira Ferreira - Mestranda em Educação/ICHS/UFOP - <a href="http://lattes.cnpq.br/5861261066577280">lattes</a></li>
+					<li>Adriana de Medeiros Oliveira - Mestranda em Educação/ICHS/UFOP - <a href="http://lattes.cnpq.br/6241686929185980">lattes</a></li>
+					<li>Ariane Dianes dos Santos Silva - Mestranda em Educação/ICHS/UFOP - <a href="http://lattes.cnpq.br/0601130582267058">lattes</a></li>
+					<li>Jianne Inês Fialho Coelho - Mestranda em Educação/ICHS/UFOP - <a href="http://lattes.cnpq.br/2669064163003279">lattes</a></li>
+				</ul>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col">
+				<p><h3>Alunos de graduação</h3></p>
+				<ul>
+					<li>Thiago Andreuci Alves Silva - História/ICHS/UFOP - <a href="http://lattes.cnpq.br/2234632720125030">lattes</a></li>
+					<li>Maria Tereza da Silva Pena - Pedagogia/ICHS/UFOP - <a href="http://lattes.cnpq.br/1282362686993205">lattes</a></li>
+					<li>Alexander Jose de Freitas - Letras/ICHS/UFOP - <a href="hhttp://lattes.cnpq.br/7842685128678132">lattes</a></li>
+					<li>Mariellen De Freitas Tioca - Economia/ICSA/UFOP - <a href="http://lattes.cnpq.br/1268614074026249">lattes</a></li>
+					<li>Tais Aguilar - Economia/ICSA/UFOP - <a href="http://lattes.cnpq.br/5706785932045955">lattes</a></li>
+					<li>Flávia Christina Joanini - Ciências Biológicas/ICEB/UFOP - <a href="http://lattes.cnpq.br/5564577026073403">lattes</a></li>
+					<li>Gustavo Henrique Trajano de Lima - História/ICHS/UFOP - <a href="http://lattes.cnpq.br/9833786960754216">lattes</a></li>
+					<li>Roberto Fernandes Ferreira - Artes Cênicas/DEART/UFOP - <a href="http://lattes.cnpq.br/0070355391163179">lattes</a></li>
+					<li>Mateus Versieux de Souza - História/ICHS/UFOP - <a href="http://lattes.cnpq.br/1367064713393841">lattes</a></li>
+					<li>​J​oao Luiz Batista Meneghini - Pedagogia/ICHS/UFOP <a herf="http://lattes.cnpq.br/5375302418960744">lattes</a></li>
+
+				</ul>
+			</div>
+		</div>
+	</div>		
