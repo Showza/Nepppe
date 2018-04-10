8ac4a8cd9ee859ac74148dd5e85d9dbfca65c9be
CamilaGit <camilatropia95@gmail.com>
2018-04-10 17:24:25 -0300

    front com maps e area admininstrativa sem o documentos

--- application/views/Contato.php
+++ application/views/Contato.php
@@ -6,7 +6,37 @@
 ​     Rua do Seminário, s/n, Mariana- MG​</p>
     
 </div>
+<div class="container">
+  <div class="row">
+    <div class="col">
+      <!-- Inicio Google Maps -->
+  <!-- Script de formatação do Mapa no arquivo Html-header linha24 -->
+      <div class="container">
+        <div id="map"></div>
+          <script>
+            function initMap() {
+              // Determina as coordenadas geograficas de Outo Preto
+              var uluru = {lat:  -20.3818, lng: -43.4150};
+              var map = new google.maps.Map(document.getElementById('map'), {
+                // Configura o zoon no Google Maps
+                zoom: 16,
+                center: uluru
+              });
+              var marker = new google.maps.Marker({
+              position: uluru,
+              map: map
+              });
+            }
+          </script>
+          <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBiYWI1GNysWk7eW5IodYU_cS1YJohtxig&callback=initMap">
+          </script>
+    </div>
+    </div>
+  </div>
 </div>
+
+
+
 
   <!-- Formulário
   <div class="row">
--- application/views/Template/Html-header.php
+++ application/views/Template/Html-header.php
@@ -22,27 +22,14 @@
          margin-top: 30px;
       }
    </style>
-<!DOCTYPE html>
-<html lang="pt-br">
 
-<head>
-   <meta charset="utf-8">
-   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-   <title>NEPPPE</title>
-   
-   <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+   <!-- Google Maps -->
+    <style>
+       #map {
+        height: 400px;
+        width: 100%;
+       }
+    </style>
 
-   <style type = "text/css">
-      div.bg{
-         background-color:  #790505;
-         height: 50px;
-         border: 1px solid #fff;
-         margin-top: 30px;
-      }
-      div.bgbody{
-         background-color:  #790505;
-         height: 50px;
-         border: 1px solid #fff;
-         margin-top: 30px;
-      }
-   </style>
+</head>
+<body>
