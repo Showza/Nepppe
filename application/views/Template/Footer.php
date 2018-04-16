4672ec0600c0036caf62fbdbf3120d25d0e7a834
CamilaGit <camilatropia95@gmail.com>
2018-04-16 17:35:14 -0300

    Footer

--- application/views/Template/Footer.php
+++ application/views/Template/Footer.php
@@ -1,4 +1,4 @@
-<!--footer-->
+<!--footer
     <div class="container-fluid">
         <div class="row">
             <div class="col">
@@ -15,6 +15,43 @@
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+-->
+
+<div class="content">
+</div>
+    <footer id="myFooter">
+        <div class="container rounded" style="background-color: #790505; font-size: 10px;">
+            <div class="row">
+                <div class="col-sm-3">
+                    <img src="<?php echo base_url('/assets/frontend/img/ufop.png');?>" width= 20% class="img-fluid" style="margin-top: 10px; margin-bottom: 5px; margin-left: 100px;">
+                </div>
+                <div class="col-sm-3" style="color:#ffffff; margin-top: 25px; margin-left: -10px; text-align: center;">
+                    <h6>Universidade Federal de Ouro Preto</h6>
+                    <p>Ouro Preto, Minas Gerais<br>
+                       CEP 35400-000</p>
+
+                </div>
+                 <div class="col-sm-3" style="color:#ffffff; margin-top: 25px; margin-left: -10px; text-align: center;">
+                    <h6>Instituto de Ciências Hunamas e Sociais</h6>
+                    <p>Mariana, Minas Gerais<br>
+                       CEP 35420-000</p>
+                </div>
+                <div class="col-sm-3" align="left">
+                    <img src="<?php echo base_url('/assets/frontend/img/ichs.png');?>" width= 70% class="img-fluid" style="margin-top: 5px; margin-bottom: 5px;">
+                </div>
+            </div>
+            <div class="col-sm-12" style="color:#ffffff;">
+                    <a class="btn btn-red btn-block" role="button" align="centered" href="http://voluta.ufop.br/" style="color: #ffffff; background-color: #790505; border-color: #790505; font-size: 10px;">&copy; Voluta Soluções Digitais, <script type="text/javascript">
+                                        var theDate=new Date()
+                                        document.write(theDate.getFullYear())
+                                        </script>
+                    </a>
+                </div>
+        </div>
+    </footer>
+
+
+
 </body>
 
 </html>
