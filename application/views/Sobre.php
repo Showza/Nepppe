ebe0b860115c4328709aacf0de52f3434ba56531
Luis <tavaresluisgu@gmail.com>
2018-04-18 15:36:52 -0300

    SiteVersaoAtual

--- application/views/Sobre.php
+++ application/views/Sobre.php
@@ -17,8 +17,8 @@
 	<div class="container" style="margin-bottom: 20px">
 		<div class="row">
 			<div class="col-12">
-				<img src="<?php echo base_url('/assets/frontend/img/ICHS.jpg');?>" class="img-fluid">
-				<center><label>Fotografia por Danúsia Natália</label></center>
+				<img src="<?php echo base_url('/assets/frontend/img/placa.jpg');?>" class="img-fluid">
+				<center><label>Fotografia por Danúsia Monteiro</label></center>
 			</div>
 		</div>
 	</div>
