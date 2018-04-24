1b29e3d514a24953891f79eb2a396a35e72eabd6
Ana Moraes <ana96moraes@gmail.com>
2018-04-24 19:32:56 -0300

    Front da Página de Pessoas conectado com o BD

--- application/views/Equipe.php
+++ application/views/Equipe.php
@@ -3,12 +3,13 @@
 			<div class="col">
 				<p><h3>Pesquisadores</h3></p>
 				<ul>
-					<li>Zara Figueiredo Tripodi - UFOP - <a href="http://lattes.cnpq.br/3705451568314846">lattes</a></li>
-					<li>Veronica Mendes Pereira - UFOP - <a href="http://lattes.cnpq.br/7345545374336076">lattes</a></li>
-					<li>Marcelo Donizete da Silva - UFOP - <a href="http://lattes.cnpq.br/7560421514245453">lattes</a></li>
-					<li>Thiago Alves - UFPR- <a href="http://lattes.cnpq.br/7802091709644694#">lattes</a></li>
-					<li>José Lucas Pedreira Bueno - UFTM - <a href="http://lattes.cnpq.br/0805268924348920">lattes</a></li>
-					<li>Breynner Ricardo de Oliveira - UFOP - <a href="http://lattes.cnpq.br/8368917322952740">lattes</a></li>
+					<?php
+					  $this->db->where('categoria', "Pesquisador");
+                      $query = $this->db->get('pessoas');
+                      foreach ($query->result() as $row) {
+                    ?>
+					<li><?php echo $row->nome . " - "; echo $row->departamento . " - "; ?> <a href="<?php echo $row->curriculo ;?>">lattes</a></li>	
+					<?php } ?>	
 				</ul>
 			</div>
 		</div>
@@ -16,10 +17,13 @@
 			<div class="col">
 				<p><h3>Alunos de mestrado</h3></p>
 				<ul>
-					<li>Eliane das Graças Pereira Ferreira - Mestranda em Educação/ICHS/UFOP - <a href="http://lattes.cnpq.br/5861261066577280">lattes</a></li>
-					<li>Adriana de Medeiros Oliveira - Mestranda em Educação/ICHS/UFOP - <a href="http://lattes.cnpq.br/6241686929185980">lattes</a></li>
-					<li>Ariane Dianes dos Santos Silva - Mestranda em Educação/ICHS/UFOP - <a href="http://lattes.cnpq.br/0601130582267058">lattes</a></li>
-					<li>Jianne Inês Fialho Coelho - Mestranda em Educação/ICHS/UFOP - <a href="http://lattes.cnpq.br/2669064163003279">lattes</a></li>
+					<?php
+					  $this->db->where('categoria', "Mestrando");
+                      $query = $this->db->get('pessoas');
+                      foreach ($query->result() as $row) {
+                    ?>
+					<li><?php echo $row->nome . " - "; echo $row->departamento . " - "; ?> <a href="<?php echo $row->curriculo ;?>">lattes</a></li>	
+					<?php } ?>	
 				</ul>
 			</div>
 		</div>
@@ -27,62 +31,13 @@
 			<div class="col">
 				<p><h3>Alunos de graduação</h3></p>
 				<ul>
-					<li>Thiago Andreuci Alves Silva - História/ICHS/UFOP - <a href="http://lattes.cnpq.br/2234632720125030">lattes</a></li>
-					<li>Maria Tereza da Silva Pena - Pedagogia/ICHS/UFOP - <a href="http://lattes.cnpq.br/1282362686993205">lattes</a></li>
-					<li>Alexander Jose de Freitas - Letras/ICHS/UFOP - <a href="hhttp://lattes.cnpq.br/7842685128678132">lattes</a></li>
-					<li>Mariellen De Freitas Tioca - Economia/ICSA/UFOP - <a href="http://lattes.cnpq.br/1268614074026249">lattes</a></li>
-					<li>Tais Aguilar - Economia/ICSA/UFOP - <a href="http://lattes.cnpq.br/5706785932045955">lattes</a></li>
-					<li>Flávia Christina Joanini - Ciências Biológicas/ICEB/UFOP - <a href="http://lattes.cnpq.br/5564577026073403">lattes</a></li>
-					<li>Gustavo Henrique Trajano de Lima - História/ICHS/UFOP - <a href="http://lattes.cnpq.br/9833786960754216">lattes</a></li>
-					<li>Roberto Fernandes Ferreira - Artes Cênicas/DEART/UFOP - <a href="http://lattes.cnpq.br/0070355391163179">lattes</a></li>
-					<li>Mateus Versieux de Souza - História/ICHS/UFOP - <a href="http://lattes.cnpq.br/1367064713393841">lattes</a></li>
-					<li>​J​oão Luiz Batista Meneghini - Pedagogia/ICHS/UFOP - <a href="http://lattes.cnpq.br/5375302418960744">lattes</a></li>
-					<li>Maria Luísa Patrocínio Souza - Pedagogia/ICHS/UFOP - <a href="http://lattes.cnpq.br/1425825324041469">lattes</a></li>
-				</ul>
-			</div>
-		</div>
-	</div>
-
-<div class="container" style="margin-top: 30px; margin-right: 30px;">
-		<div class="row">
-			<div class="col">
-				<p><h3>Pesquisadores</h3></p>
-				<ul>
-					<li>Zara Figueiredo Tripodi - UFOP - <a href="http://lattes.cnpq.br/3705451568314846">lattes</a></li>
-					<li>Veronica Mendes Pereira - UFOP - <a href="http://lattes.cnpq.br/7345545374336076">lattes</a></li>
-					<li>Marcelo Donizete da Silva - UFOP - <a href="http://lattes.cnpq.br/7560421514245453">lattes</a></li>
-					<li>Thiago Alves - UFPR- <a href="http://lattes.cnpq.br/7802091709644694#">lattes</a></li>
-					<li>José Lucas Pedreira Bueno - UFTM - <a href="http://lattes.cnpq.br/0805268924348920">lattes</a></li>
-					<li>Breynner Ricardo de Oliveira - UFOP - <a href="http://lattes.cnpq.br/8368917322952740">lattes</a></li>
-				</ul>
-			</div>
-		</div>
-		<div class="row">
-			<div class="col">
-				<p><h3>Alunos de mestrado</h3></p>
-				<ul>
-					<li>Eliane das Graças Pereira Ferreira - Mestranda em Educação/ICHS/UFOP - <a href="http://lattes.cnpq.br/5861261066577280">lattes</a></li>
-					<li>Adriana de Medeiros Oliveira - Mestranda em Educação/ICHS/UFOP - <a href="http://lattes.cnpq.br/6241686929185980">lattes</a></li>
-					<li>Ariane Dianes dos Santos Silva - Mestranda em Educação/ICHS/UFOP - <a href="http://lattes.cnpq.br/0601130582267058">lattes</a></li>
-					<li>Jianne Inês Fialho Coelho - Mestranda em Educação/ICHS/UFOP - <a href="http://lattes.cnpq.br/2669064163003279">lattes</a></li>
-				</ul>
-			</div>
-		</div>
-		<div class="row">
-			<div class="col">
-				<p><h3>Alunos de graduação</h3></p>
-				<ul>
-					<li>Thiago Andreuci Alves Silva - História/ICHS/UFOP - <a href="http://lattes.cnpq.br/2234632720125030">lattes</a></li>
-					<li>Maria Tereza da Silva Pena - Pedagogia/ICHS/UFOP - <a href="http://lattes.cnpq.br/1282362686993205">lattes</a></li>
-					<li>Alexander Jose de Freitas - Letras/ICHS/UFOP - <a href="hhttp://lattes.cnpq.br/7842685128678132">lattes</a></li>
-					<li>Mariellen De Freitas Tioca - Economia/ICSA/UFOP - <a href="http://lattes.cnpq.br/1268614074026249">lattes</a></li>
-					<li>Tais Aguilar - Economia/ICSA/UFOP - <a href="http://lattes.cnpq.br/5706785932045955">lattes</a></li>
-					<li>Flávia Christina Joanini - Ciências Biológicas/ICEB/UFOP - <a href="http://lattes.cnpq.br/5564577026073403">lattes</a></li>
-					<li>Gustavo Henrique Trajano de Lima - História/ICHS/UFOP - <a href="http://lattes.cnpq.br/9833786960754216">lattes</a></li>
-					<li>Roberto Fernandes Ferreira - Artes Cênicas/DEART/UFOP - <a href="http://lattes.cnpq.br/0070355391163179">lattes</a></li>
-					<li>Mateus Versieux de Souza - História/ICHS/UFOP - <a href="http://lattes.cnpq.br/1367064713393841">lattes</a></li>
-					<li>​J​oão Luiz Batista Meneghini - Pedagogia/ICHS/UFOP - <a href="http://lattes.cnpq.br/5375302418960744">lattes</a></li>
-					<li>Maria Luísa Patrocínio Souza - Pedagogia/ICHS/UFOP - <a href="http://lattes.cnpq.br/1425825324041469">lattes</a></li>
+					<?php
+					  $this->db->where('categoria', "Graduando");
+                      $query = $this->db->get('pessoas');
+                      foreach ($query->result() as $row) {
+                    ?>
+					<li><?php echo $row->nome . " - "; echo $row->departamento . " - "; ?> <a href="<?php echo $row->curriculo ;?>">lattes</a></li>	
+					<?php } ?>	
 				</ul>
 			</div>
 		</div>
