47d784b8c9850761c95792c09fb71c18a5d97403
fctof <fctofani2@gmail.com>
2018-11-10 09:34:48 -0200

    Basedados.php

--- application/views/Basedados.php
+++ application/views/Basedados.php
@@ -56,11 +56,11 @@ Começar a Segunda Aba
 		<div id="accordion">
 	<?php
 		$estados = array(
-			"Acre","Alagoas","Amapá","Amazonas","Bahia","Ceará","Distrito Federal","Espirito Santo","Goiás","Maranhão",
+			"Acre","Alagoas","Amapá","Amazonas","Bahia","Ceará","Distrito Federal","Espirito Sato","Goiás","Maranhão",
 			"Mato Grosso","Mato Grosso do Sul","Minas Gerais","Pará","Paraíba","Paraná","Pernambuco","Piauí",
 			"Rio de Janeiro","Rio Grande do Norte","Rio Grande do Sul","Rondônia","Roraima","Santa Catarina",
 			"São Paulo","Sergipe","Tocantins",
-		);
+		)
 	 	$legenda = array(
 			"AC","AL","AP","AM","BA","CE","DF","ES","GO","MA","MT","MS","MG","PA","PB","PR","PE","PI","RJ","RN","RS",
 			"RO","RR","SC","SP","SE","TO",
